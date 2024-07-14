6b46334a45625520e80782fa1167c6ea6224a8e8
Boris <26038255+okplanbo@users.noreply.github.com>
2024-07-15 02:19:59 +0400

    Update vite.config.js
    add base url

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+import { name } from './package.json'
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: `/${name}/`,
   plugins: [react()],
 });
