e0ed29f887bc96697d17117d17b2ff6669b51a33
okplanbo <26038255+okplanbo@users.noreply.github.com>
2024-07-16 00:03:32 +0400

    new: move img to public, test img render

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 /** TBD:
-*** add translated json and images
+*** add translated json
 *** add routing with pages: start view, test view, filtered table with questions
 *** i18n?
 *** ts support?
@@ -15,15 +13,8 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-row items-center">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-4xl font-bold">Hello World</h1>
+      <img src="img1.avif" width="300" className="ml-auto mr-auto mb-6" alt="" />
+      <h1 className="text-4xl font-bold">B1 theory exam practice</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
